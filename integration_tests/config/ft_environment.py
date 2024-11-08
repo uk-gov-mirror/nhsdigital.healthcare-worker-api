3d24c403d35e5fa911814f093876d570912d2263
ian-robinson-27 <151538887+ian-robinson-27@users.noreply.github.com>
2024-11-08 14:45:20 +0000

    HCW-85: Gitleak updates (#29)

--- integration_tests/config/ft_environment.py
+++ integration_tests/config/ft_environment.py
@@ -2,7 +2,7 @@ from integration_tests.config.base_environment import EnvironmentConfig
 
 
 class FtEnvironmentConfig(EnvironmentConfig):
-    client_id = "kqlKbRU2HaGYOR2T9Tpy2aZtBaMwkqAO"
+    client_id = "c3Uk5zOnUVJh5aUSgI2FfrqA758vUWiE"
 
     def __init__(self):
         super(FtEnvironmentConfig, self).__init__(self.client_id)
