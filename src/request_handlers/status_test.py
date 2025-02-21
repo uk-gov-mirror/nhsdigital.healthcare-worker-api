038d8c7536d61b14e86d178a7eb80c1f018754d1
Ian Robinson <ian.robinson27@nhs.net>
2025-02-21 15:26:26 +0000

    Fix tests

--- src/request_handlers/status_test.py
+++ src/request_handlers/status_test.py
@@ -1,11 +1,20 @@
+from unittest.mock import patch
+
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
 
 from request_handlers.status import StatusHandler
 
-
-def test_status_handler():
+@patch("request_handlers.status.get_connection")
+def test_status_handler(_):
     handler = StatusHandler()
 
     response = handler.get(APIGatewayProxyEvent(data={}))
 
     assert response.main_response[0].ok is True
+
+def test_status_handler_with_failed_ldap():
+    handler = StatusHandler()
+
+    response = handler.get(APIGatewayProxyEvent(data={}))
+
+    assert response.main_response[0].ok is False
