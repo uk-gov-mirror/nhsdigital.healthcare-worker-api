fc08ade5e8a2e17c87cdb1a8819b9010bd1fa386
Jolyon Brown <jolyon.brown@nhs.net>
2025-02-28 17:03:40 +0000

    hcw-239-new-data-source-connection fix security group issue

--- infrastructure/modules/vpc/security_groups.tf
+++ infrastructure/modules/vpc/security_groups.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "ldap_endpoint" {
     from_port       = 636
     to_port         = 636
     protocol        = "tcp"
-    security_groups = [aws_security_group.vpc_lambda.id]
+    prefix_list_ids = [aws_vpc_endpoint.vpc_lambda_to_cis1_ldap.prefix_list_id]
     description     = "LDAPS calls to SDS"
   }
 
