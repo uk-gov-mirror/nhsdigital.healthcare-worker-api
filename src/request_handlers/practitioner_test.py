49ec9bd18923f5d3e2fddd2ae6315d920ceb12ac
Sat Thakor <satyarajt@yahoo.com>
2025-03-05 12:12:57 +0000

    [HCW-229] Updated nhsOrgOpenDate attr as per cis1 directory

--- src/request_handlers/practitioner_test.py
+++ src/request_handlers/practitioner_test.py
@@ -93,12 +93,12 @@ def mock_ldap(uid: Optional[str] = "uid", sn: Optional[str] = "Smith",
         "nhsIDCode": "id-code",
         "nhsSiteNames": ["site-1", "site-2"],
         "nhsSiteCodes": ["1", "2"],
-        "nhsOpenDate": "20200101",
+        "nhsOrgOpenDate": "20200101",
         "nhsCloseDate": "30000101"
     })
     nhs_org_person = NhsOrgPerson({
         "uniqueIdentifier": "org-person-id",
-        "nhsOpenDate": "20000101",
+        "nhsOrgOpenDate": "20000101",
         "nhsIDCode": "id-code",
         "o": "nhs",
     })
