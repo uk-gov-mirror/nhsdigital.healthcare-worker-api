deaee7d48828ae8fa46136bdb1c3ca3e2ea0e624
Jolyon Brown <jolyon.brown@nhs.net>
2025-09-04 15:51:59 +0100

    HCW-283 added role checks for missing nhsOrgOpenDate coverage

--- integration_tests/example_practitioners.py
+++ integration_tests/example_practitioners.py
@@ -59,7 +59,7 @@ practitioners: [PractitionerExample] = [
                         "Miss Shayla Cailin Seanna Cayley Ashworth", []),
     PractitionerExample(MISSING_ORG_PERSON_DATES, "Dr", "LOUISE", "STEENE", "Dr LOUISE STEENE", []),
     PractitionerExample(MISSING_ORG_PERSON_OPEN_PAST_CLOSE, "Mr", "KARTHIK J", "BVM", "Mr KARTHIK J BVM", []),
-    PractitionerExample(MISSING_ROLE_OPEN_DATE_PAST_CLOSE, "Mr", "Elvis", "Presley", "Mr Elvis Presley", []),
+    PractitionerExample(MISSING_ROLE_OPEN_DATE_PAST_CLOSE, "Mr", "Elvis", "Presley", "Mr Elvis Presley",
                         [RoleExample("100006095936", "Q32",
                                         "", "",
                                         "S0080:G0450:R0040",
