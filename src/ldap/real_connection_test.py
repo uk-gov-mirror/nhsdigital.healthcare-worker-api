f8216bb27228bf93f3217c08216a85ef1d555b31
Jolyon Brown <jolyon.brown@nhs.net>
2025-09-25 18:50:45 +0100

    HCW-286 fix failing test

--- src/ldap/real_connection_test.py
+++ src/ldap/real_connection_test.py
@@ -278,7 +278,11 @@ def test_connect():
             validate=CERT_REQUIRED)
 
         server.assert_called_with("gateway_url",
-            use_ssl=True, tls=tls.return_value)
+            use_ssl=True,
+            tls=tls.return_value,
+            connect_timeout=4,
+            get_info=NONE,
+            mode=IP_V4_ONLY)
 
         connection.assert_called_with(server.return_value, user="username", password="password",
                                         client_strategy=SAFE_SYNC, receive_timeout=8, pool_keepalive=8)
